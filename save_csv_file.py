c95f7890ea1b32e869a37213fbf3a0d83b93a109
DorofeevAlexandr <amd_86@mail.ru>
2023-10-18 22:34:18 +0300

    Шапку csv файла записываем 1 раз в час

--- save_csv_file.py
+++ save_csv_file.py
@@ -8,13 +8,14 @@ from wear_lines import read_data_in_base
 app = create_app()
 
 
-def append_in_csv(lines):     
+def append_in_csv(lines, write_header=True):     
     shift_hours = 8   
     file_name = create_dir(shift_hours=8)
     with open(file_name, mode="a", encoding='utf-8') as w_file:
         file_writer = csv.writer(w_file, delimiter = ";", lineterminator=";\r")
         st_time = str((datetime.now() - timedelta(hours=shift_hours)).hour) + ':' + str(datetime.now().minute) + ':' + str(datetime.now().second)
-        file_writer.writerow(['time'] + [line['name'] for line in lines])
+        if write_header:
+            file_writer.writerow(['time'] + [line['name'] for line in lines])
         file_writer.writerow([st_time] + [str(int(line['length'] * 1000)) for line in lines])
 
 
@@ -35,8 +36,14 @@ def create_dir(shift_hours=0):
 
 if __name__ == '__main__':
     with app.app_context():
+        write_header = True
+        header_writed_hour = None
         while True:
+            if header_writed_hour != datetime.now().hour:
+                header_writed_hour = datetime.now().hour 
+                write_header = True
             lines = read_data_in_base()
-            append_in_csv(lines)
+            append_in_csv(lines, write_header)
+            write_header = False
             # break
-            _time.sleep(56)
+            _time.sleep(60)
